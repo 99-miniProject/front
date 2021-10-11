df973252556bee47f994694727489a24e67e818b
kyuung <mel1521@naver.com>
2021-10-11 18:38:49 +0900

    ➕ Add, Update Components ( for MainPage )

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+	return <>footer입니당</>;
+};
+
+export default Footer;
---
+++ src/components/MainFilter.js
@@ -0,0 +1,74 @@
+// ! kyuung
+import React from 'react';
+import styled from 'styled-components';
+
+import { Button, Text } from '../elements/index';
+
+const MainFilter = () => {
+	return (
+		<>
+			<section>
+				<div>
+					<Button
+						width="6rem"
+						height="3rem"
+						fontColor="white"
+						bgColor="black"
+					>
+						필터1
+					</Button>
+					<Button
+						width="6rem"
+						height="3rem"
+						fontColor="white"
+						bgColor="black"
+					>
+						필터2
+					</Button>
+					<Button
+						width="6rem"
+						height="3rem"
+						fontColor="white"
+						bgColor="black"
+					>
+						필터3
+					</Button>
+					<Button
+						width="6rem"
+						height="3rem"
+						fontColor="white"
+						bgColor="black"
+					>
+						필터4
+					</Button>
+					<Button
+						width="6rem"
+						height="3rem"
+						fontColor="white"
+						bgColor="black"
+					>
+						필터5
+					</Button>
+					<Button
+						width="6rem"
+						height="3rem"
+						fontColor="white"
+						bgColor="black"
+					>
+						필터6
+					</Button>
+					<Button
+						width="6rem"
+						height="3rem"
+						fontColor="white"
+						bgColor="black"
+					>
+						필터7
+					</Button>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default MainFilter;
