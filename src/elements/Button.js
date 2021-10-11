e9a7e173dcaf27287a0e4915480bd13f4db87703
kyuung <mel1521@naver.com>
2021-10-11 17:55:46 +0900

    ➕ Add Elements ( Input, Button )

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,7 +1,53 @@
+// ! kyuung
 import React from 'react';
+import styled from 'styled-components';
 
-const Button = () => {
-	return <div></div>;
+const Button = (props) => {
+	const {
+		width,
+		height,
+		fontColor,
+		bgColor,
+		fontSize,
+		_onClick,
+		otherStyles,
+		children,
+	} = props;
+	const styles = {
+		width,
+		height,
+		fontColor,
+		bgColor,
+		fontSize,
+		otherStyles,
+	};
+
+	return (
+		<>
+			<ElButton {...styles} onClick={_onClick}>
+				{children}
+			</ElButton>
+		</>
+	);
 };
+
+Button.defaultProps = {
+	width: '4rem',
+	height: '2rem',
+	fontColor: 'white',
+	bgColor: 'black',
+	fontSize: '14px',
+	_onClick: () => {},
+};
+
+const ElButton = styled.button`
+	width: ${(props) => props.width};
+	height: ${(props) => props.height};
+	color: ${(props) => props.fontColor};
+	background-color: ${(props) => props.bgColor};
+	font-size: ${(props) => props.fontSize};
+	${(props) => props.otherStyles};
+	border: none;
+`;
 
 export default Button;
--- src/elements/Input.js
+++ src/elements/Input.js
@@ -1,7 +1,47 @@
+// ! kyuung
 import React from 'react';
+import styled from 'styled-components';
 
-const Input = () => {
-	return <></>;
+const Input = (props) => {
+	const {
+		width,
+		height,
+		fontSize,
+		otherStyles,
+		_onChange,
+		placeholder,
+		text,
+	} = props;
+	const styles = { width, height, fontSize, otherStyles };
+
+	return (
+		<>
+			<label>{text}</label>
+			<ElInput
+				{...styles}
+				onChange={_onChange}
+				placeholder={props.placeholder}
+			/>
+		</>
+	);
 };
+
+Input.defaultProps = {
+	width: '100%',
+	height: '3rem',
+	fontSize: '14px',
+	_onChange: () => {},
+	placeholder: 'placeholder',
+	text: 'label',
+};
+
+const ElInput = styled.input`
+	width: ${(props) => props.width};
+	height: ${(props) => props.height};
+	font-size: ${(props) => props.fontSize};
+	${(props) => props.otherStyles};
+	padding-left: 0.5rem;
+	margin-top: 1rem;
+`;
 
 export default Input;
