03a128e85d6d813cf5afe4fc3808392dc43add50
kyuung <mel1521@naver.com>
2021-10-12 17:00:38 +0900

    ➕ Add props

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,43 +1,60 @@
 // ! kyuung
 import React from 'react';
+import { useHistory } from 'react-router';
 import styled from 'styled-components';
 import { Grid, Image, Text, Button } from '../elements/index';
 
 const MainCard = (props) => {
-	const { camping_name, camping_price } = props;
+	const { camp_id, camp_name, camp_price, camp_src } = props;
+	const history = useHistory();
+
 	return (
 		<>
-			<Grid
-				fd="column"
-				ai="left"
-				width="auto"
-				others="margin-bottom:2rem;"
+			<PointerDiv
+				styles={{ cursor: 'pointer' }}
+				onClick={() => {
+					history.push(`/post/${camp_id}`);
+				}}
 			>
-				<Image
-					others="-webkit-box-shadow: 5px 7px 12px 0px rgba(0,0,0,0.78); 
-							box-shadow: 5px 7px 12px 0px rgba(0,0,0,0.78);margin-bottom:1.3rem;"
-					bradius="18px"
-				/>
-				<Text bold="700" fontSize="1.2rem">
-					{camping_name}
-				</Text>
-				<Text>{camping_price}</Text>
-				<Button
-					width="5rem"
-					height="2.2rem"
-					bradius="12px"
-					others="margin-top:0.7rem; &:hover{background-color:white;color:black;border:1px solid gray;}"
+				<Grid
+					fd="column"
+					ai="left"
+					width="auto"
+					others="margin-bottom:2rem;"
 				>
-					<span styles={{ fontWeight: 700 }}>예약</span>
-				</Button>
-			</Grid>
+					<Image
+						others="-webkit-box-shadow: 5px 7px 12px 0px rgba(0,0,0,0.78); 
+							box-shadow: 5px 7px 12px 0px rgba(0,0,0,0.78);margin-bottom:1.3rem;"
+						bradius="18px"
+						src={camp_src}
+					/>
+					<Text bold="700" fontSize="1.2rem">
+						{props.camp_name}
+					</Text>
+					<Text>{props.camp_price}</Text>
+					<Button
+						width="5rem"
+						height="2.2rem"
+						bradius="12px"
+						others="margin-top:0.7rem; &:hover{background-color:white;color:black;border:1px solid gray;}"
+					>
+						<span styles={{ fontWeight: 700 }}>예약</span>
+					</Button>
+				</Grid>
+			</PointerDiv>
 		</>
 	);
 };
 
 MainCard.defaultProps = {
-	camping_name: '애옹이 캠핑장',
-	camping_price: '55,000 ~ 99,000',
+	camp_id: 1,
+	camp_name: '숲 캠핑장',
+	camp_price: '85,000 ~ 100,000',
+	camp_src: 'https://my-speak-app.s3.ap-northeast-2.amazonaws.com/camp.jpg',
 };
+
+const PointerDiv = styled.div`
+	cursor: pointer;
+`;
 
 export default MainCard;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,15 +6,31 @@ import { Header, Footer, MainFilter, MainCard } from '../components/index';
 import { Input, Button, Text, Grid, Image } from '../elements/index';
 
 const MainPage = (props) => {
-	const campingDats = [
-		{ name: '애옹이 캠핑장 1', price: '55,000 ~ 99,000' },
-		{ name: '애옹이 캠핑장 2', price: '77,000 ~ 111,000' },
-		{ name: '애옹이 캠핑장 3', price: '85,000 ~ 100,000' },
-		{ name: '애옹이 캠핑장 4', price: '85,000 ~ 100,000' },
-		{ name: '애옹이 캠핑장 5', price: '85,000 ~ 100,000' },
-		{ name: '애옹이 캠핑장 6', price: '85,000 ~ 100,000' },
-		{ name: '애옹이 캠핑장 7', price: '85,000 ~ 100,000' },
-		{ name: '애옹이 캠핑장 8', price: '85,000 ~ 100,000' },
+	const resCampingData = [
+		{
+			id: 1,
+			name: '숲 캠핑장 1',
+			price: '55,000 ~ 99,000',
+			src: 'https://my-speak-app.s3.ap-northeast-2.amazonaws.com/camp.jpg',
+		},
+		{
+			id: 2,
+			name: '애옹이 캠핑장 2',
+			price: '77,000 ~ 111,000',
+			src: 'https://my-speak-app.s3.ap-northeast-2.amazonaws.com/cat1.jpeg',
+		},
+		{
+			id: 3,
+			name: '숲 캠핑장 3',
+			price: '85,000 ~ 100,000',
+			src: 'https://my-speak-app.s3.ap-northeast-2.amazonaws.com/camp.jpg',
+		},
+		{
+			id: 4,
+			name: '애옹이 캠핑장 4',
+			price: '85,000 ~ 100,000',
+			src: 'https://my-speak-app.s3.ap-northeast-2.amazonaws.com/cat1.jpeg',
+		},
 	];
 
 	return (
@@ -38,11 +54,14 @@ const MainPage = (props) => {
 					height="50vh"
 					others="flex-wrap:wrap"
 				>
-					{campingDats.map((data, idx) => (
+					{resCampingData.map((data, idx) => (
 						<MainCard
 							index={idx}
-							camping_name={data.name}
-							camping_price={data.price}
+							camp_id={data.id}
+							camp_name={data.name}
+							camp_price={data.price}
+							camp_src={data.src}
+							a="abc"
 						/>
 					))}
 				</Grid>
